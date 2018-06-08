86b81500394d9e4ec4a3c04ba6c0ca925370dacc
Gary <gary003>
2018-06-08 22:36:16 +0200

    maj

--- facto_optim.js
+++ facto_optim.js
@@ -1,9 +1,9 @@
-const facto = require('./facto'); //?
+const facto = require('./facto')
 
 for (var i = 0; i < 10000; i++) {
   facto.rec(150)/*?.*/
 }
-for (var i = 0; i < 10000; i++) {//?
+for (var i = 0; i < 10000; i++) {
   facto.hhf(150)/*?.*/
 }
 for (var i = 0; i < 10000; i++) {
@@ -12,6 +12,3 @@ for (var i = 0; i < 10000; i++) {
 for (var i = 0; i < 10000; i++) {
   facto.rec_ter(150)//?.
 }
-
-facto.rec_ter(30) //?.
-facto.rec(30) //?.
--- push_function.js
+++ push_function.js
@@ -4,11 +4,13 @@ const pushy = (...values) => {
   // In case the user passes an array of args instead of a list of args
   values = _.flatten(values)
 
-  console.log(values)
+  console.log(values[0])
 
   values.shift()
+
   if(values.length > 0)
     pushy(...values)
+
 }
 
 pushy("JS","ruby","PHP","java","kotlin")
