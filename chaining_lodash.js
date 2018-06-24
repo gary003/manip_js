19daf34c3b66e80be5c80554150ffa9e3510b687
Gary <gary003>
2018-06-24 10:41:00 +0200

    comparaison lodash-vanilla

--- chaining_lodash.js
+++ chaining_lodash.js
@@ -1,36 +1,56 @@
 const _ = require('lodash')
 
-tab = ["13",null,null,34,6,-54,undefined,'345',7,98]
+tab = ["13",null,null,34,'-345',7,-98,6,-54,undefined,'345',7,98]
 
-const maximum =
-       _.chain(tab)
+const groupVanillaJS =
+        tab
         .filter((val) => ! [undefined,null].includes(val))
         .map((val) => parseInt(val))
-        .slice(1,5)
+        .slice(1,9)
+        .reduce( (group,v) => {
+          let a = Math.sign(v)
+          group[a] = group[a] || []
+          group[a].push(v)
+          return group
+        },{})
+
+const groupLodashJS =
+        _(tab)
+        .filter((val) => ! [undefined,null].includes(val))
+        .map((val) => parseInt(val))
+        .slice(1,9)
+        .groupBy(Math.sign)
         .value()
 
-maximum
+const gLo = groupLodashJS //?.
+const gJS = groupVanillaJS //?.
+
+console.log(gLo)
+console.log(gJS)
 
 const s = "azerty poiuytrre ipsuuu teea cup pick up"
 
-const maxOccurrence =  _.chain(s)
-                        .split('')
-                        .countBy(o => o == "a")
-                        .value()
+const maxOccurrence = (letter) => {
+  return _.chain(s)
+    .split('')
+    .countBy(o => o == letter)
+    .value()
+    .true
+}
 
-maxOccurrence
+console.log(maxOccurrence("u"))
 
 const max_letter = () => {
   return new Promise( (resolve,reject) => {
     return resolve(
-            _(s)
-              .split('')
-              .countBy()
-              .thru( value => {
-                const maxi = _(value).chain().values().max().value()
-                return _.pickBy(value,(v,k) => v == maxi )
-              })
-              .value()
+      _(s)
+      .split('')
+      .countBy()
+      .thru( myself => {
+        const maxi = _(myself).chain().values().max().value()
+        return _.pickBy(myself,(v,k) => v == maxi )
+      })
+      .value()
     )
   })
 }
