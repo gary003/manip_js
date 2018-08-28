245e7e1d5bc44af8bc8f340344e2fb61f66c8dce
garyupsell <gary003>
2018-08-28 15:45:58 +0200

    test map avec bluebird

--- flatMap.js
+++ flatMap.js
@@ -1,22 +1,23 @@
-const fs = require('fs')
-const _ = require('lodash')
+const fs = require("fs");
+const _ = require("lodash");
+const Prom = require("bluebird");
 
 const fetch_file = file => {
-  return new Promise((res,rej) => {
-    fs.readFile(file,'utf8',(err,data) => {
-      return err? rej(err) : res(data)
-    })
-  })
-}
+  return new Promise((res, rej) => {
+    fs.readFile(file, "utf8", (err, data) => {
+      return err ? rej(err) : res(data);
+    });
+  });
+};
 
-const files = ["files/lorem.txt",'files/conf.json']
-const res =
-  _(files)
-  .flatMap(f => fetch_file(__dirname+'/'+f))
-  .flatMap(f => f)
-  .value()
+const files = ["files/lorem.txt", "files/ascii.txt"];
+// const res =
+//   _(files)
+//   .map(f => fetch_file(__dirname+'/'+f))
+//   .value()
 
-res
+const res = Prom.map(files, f => fetch_file(__dirname + "/" + f));
+res 
 
 // Promise.all(res)
 //   .then((r) => console.log(r))
