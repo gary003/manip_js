a5153eb3d353a5383ed65d33742da33de4c63e10
Gary Johnson <gary.johnson@upsell.fr>
2018-10-24 23:10:44 +0200

    test promise delay && consoleLog

---
+++ consoleLog.js
@@ -0,0 +1,12 @@
+const tab = [
+  {l:12 , a:33 , p:73},
+  {l:43 , a:98 , p:25},
+  {l:1 , a:76 , p:41}
+]
+
+// log avec nom de la variable
+console.log({tab});
+
+// timer 
+console.time("timerTest")
+setTimeout(() => console.timeEnd("timerTest") , 2500)
