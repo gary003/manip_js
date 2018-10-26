af776b2685c29fe075b6f0f8c8a43c1a38ce650b
Gary Johnson <gary.johnson@upsell.fr>
2018-10-26 16:49:07 +0200

    MAJ

--- currying.js
+++ currying.js
@@ -1,3 +1,13 @@
 const volumeCube = (len) => (wid) => (height) => len * wid * height
 
-console.log(volumeCube(5)(5)(5))
+console.log(volumeCube(5))
+
+const ii = async (a,b) => a * b
+
+Promise.all([
+  ii(4,5),
+  ii(12,5)
+])
+.then( res => console.log(res))
+.catch(e => e)
+
