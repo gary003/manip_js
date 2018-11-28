51e156f854eaf9fde302c0fc6f1074edd7ee1e15
Gary Johnson <gary.johnson@upsell.fr>
2018-11-28 12:33:50 +0100

    meilleur destructuring

--- destructuring.js
+++ destructuring.js
@@ -1,9 +1,31 @@
-makeSound({
-  species:'dog',
-  weight:34,
-  sound:'woof'
+const _ = require('lodash')
+
+// makeSound({
+//   species:'dog',
+//   weight:34,
+//   sound:'woof'
+// })
+
+// function makeSound({species,sound}){
+//   console.log(species+' says '+sound);
+// }
+
+const box = {
+  'content' : ['screwdriver' , 'screw' , 'hammer'] ,
+  'weight' : 12 ,
+  'height' : 11 ,
+  'color'  : 'red' 
+}
+
+const { content , ...metadata } = box  
+
+const inside = content.map(x => {
+  return _.capitalize(x)
 })
 
-function makeSound({species,sound}){
-  console.log(species+' says '+sound);
-}
+console.log(inside);
+console.log(metadata);
+ 
+const new_box = { inside , ...metadata }
+
+console.log(new_box);
