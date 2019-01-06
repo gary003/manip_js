64d004510df897749e58c0805725f0fff42b4df4
Gary Johnson <gary.johnson@upsell.fr>
2019-01-06 14:53:11 +0100

    ajout nodeC++ addon

--- fakerJS.js
+++ fakerJS.js
@@ -4,11 +4,11 @@ const _ = require('lodash')
 const tabs = [[[2,43,23,54,4]]]
 
 console.log(tabs)
-console.log(tabs.flatten);
+console.log(_.flattenDeep(tabs));
 
+const firstname = faker.name.firstName()
+firstname
 
-
-// const firstname = faker.name.firstName()
 // const lastname = faker.name.lastName()
 // let date_f = new Date(faker.date.past())
 // date_f = Date.parse(date_f)
