1a395e34748c4caa6f2127b3a2064a6693045422
Gary Johnson <gary.johnson@upsell.fr>
2019-01-16 20:11:45 +0100

    MAJ compo

--- mailer_test/mailer.js
+++ mailer_test/mailer.js
@@ -1,23 +1,21 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
-const fs = require('fs');
+const express = require('express')
+const nodemailer = require('nodemailer')
+const fs = require('fs')
 const bodyParser = require('body-parser')
 const path = require('path')
 
 const app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 const conf = fs.readFileSync(path.join(__dirname,"conf.json") , 'utf8')
 const config = JSON.parse(conf)
 
-// Dont forget to set your mail accout to allow 'less secured apps' to connect 
+// Dont forget to set your mail account to allow 'less secured apps' to connect
 let transporter = nodemailer.createTransport({
   service: config.service_mail,
   auth: {
@@ -36,7 +34,7 @@ app.post('/send', (req, res) => {
     subject: req.body.title,
     text: req.body.message,
     html: req.body.message
-  };
+  }
 
   return transporter.sendMail(mailOptions,(err, response) => {
     if(!!err){
@@ -44,9 +42,7 @@ app.post('/send', (req, res) => {
       return res.status(500).redirect('/');
     }
     return res.status(200).redirect('/');
-  });
+  })
 })
 
-app.listen(8080, (err) => {
-  console.log('listen on 8080');
-})
+app.listen(8080, (err) => console.log('listen on 8080'))
