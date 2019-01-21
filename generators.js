1393171a590e072d6c3711cb9f02ae2ad8bb241f
Gary Johnson <gary.johnson@upsell.fr>
2019-01-21 18:01:25 +0100

    MAJ

--- generators.js
+++ generators.js
@@ -4,7 +4,6 @@ const sizes = ["tiny","little","medium","big","huge"]
 const astres = ["sun","comet","planet","nova"]
 
 const iteraMaker = function* () {
-  let ee
   while (Math.random() < 0.8) {
     let size = sizes[Math.floor((Math.random() * sizes.length))]
     let astretype = astres[Math.floor((Math.random() * astres.length))]
@@ -14,8 +13,5 @@ const iteraMaker = function* () {
 
 const iter = iteraMaker()
 
-let ii
-do{
-  ii = iter.next()
-  !ii.done ? console.log(ii) : ii
-}while(!ii.done)
+for(spaceObj of iter)
+  console.log(spaceObj)
