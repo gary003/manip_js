a64e1feb86f1ecac0f3173cadcf854c5bb40c0be
gary003 <gary.johnson003@gmail.com>
2020-04-26 14:15:54 +0200

    MAJ

--- combination.js
+++ combination.js
@@ -1,21 +1,4 @@
-const combinationCouple = async (arr , nEnd = arr.length-1 , nInit = 0, allHands = []) => {
-  if(!arr.length) return Promise.reject(res)
-  if(nEnd == nInit) return Promise.resolve()
-
-  if(arr[nInit] != arr[nEnd])
-    res.push([arr[nInit],arr[nEnd]].sort())
-
-  await combination(arr, nEnd, nInit+1, res).catch(err => console.log(err))
-  await combination(arr, nEnd-1, nInit, res).catch(err => console.log(err))
-  
-  res = res.map(val => JSON.stringify(val))
-           .filter((val,i,a) => a.indexOf(val) == i)
-           .map(x => JSON.parse(x))
-
-  return Promise.resolve(res)
-}
-
-const combination = (cards, hand = [], allHands = [], shift = false) => {
+const combinations = (cards, hand = [], allHands = [], shift = false) => {
   if(cards.length == 0) return null
 
   const [card, ...rest] = cards
@@ -23,9 +6,9 @@ const combination = (cards, hand = [], allHands = [], shift = false) => {
   if(shift) allHands.push(hand)
 
   if(hand.filter(x => x == card).length < cards.filter(x => x == card).length)
-    combination(cards, hand.concat([card]),allHands , true) 
+    combinations(cards, hand.concat([card]),allHands , true) 
   
-  combination(rest, hand, allHands,false)
+  combinations(rest, hand, allHands,false)
   
   return allHands
 }
@@ -40,8 +23,7 @@ function* iCombinations2rec(coins,change = [], shift = false){
   if(change.filter(x => x == coin).length < coins.filter(x => x == coin).length)
     yield* [...iCombinations2rec(coins, change.concat([coin]) ,true)]
       
-  yield* [...iCombinations2rec(rest, change, false)]
-    
+  yield* [...iCombinations2rec(rest, change, false)]  
 }
 
 function* iCombinations1rec(arr, k,change = Array.from(arr)){
@@ -55,8 +37,9 @@ function* iCombinations1rec(arr, k,change = Array.from(arr)){
 }
 
 console.time('e')
-const comb = iCombinations2rec(['♠A','♠2','♠8','♠J','♠9','♠K','♠7','♠4','♠Q','♠J','♠2','♠A','♠9','♠Q','♠J','♠8'])
-const s = [...comb]
-console.log(s);
+const comb = combinations(['♠A','♠2','♠K','♠8','♠5','♠Q','♠9'])
+// const s = [...comb]
+// console.log(comb);
+// console.log(s);
 
 console.timeEnd('e')
