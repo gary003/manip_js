1d460c68749fef505bd21582f1cbc89b136a51a2
gary003 <gary.johnson003@gmail.com>
2021-07-24 10:55:08 +0200

    make change is now very DP

--- makeChange.js
+++ makeChange.js
@@ -13,7 +13,7 @@ const changeRec = (amount, coins, change = [], res = []) => {
 
 const changeTab = (money, coins) => {
   const res = Array(money + 1).fill([])
-  res[0] = [[0]]
+  res[0] = [[]]
 
   for (let iMoney = 0; iMoney <= money; iMoney++) {
     if (res[iMoney].length > 0) {
@@ -27,30 +27,30 @@ const changeTab = (money, coins) => {
     }
   }
 
-  return res[money].map((change) => change.slice(1))
+  return res[money]
 }
 
 const changeTab2 = (money, coins) => {
   const res = Array(money + 1).fill([])
-  res[0] = [[0]]
+  res[0] = [[]]
 
-  for (let iCoin = 0; iCoin < coins.length; iCoin++)
-    for (let iMoney = coins[iCoin]; iMoney <= money; iMoney++) {
+  for (let coin of coins)
+    for (let iMoney = coin; iMoney <= money; iMoney++) {
       // this variable contains the subProblem for the current iMoney
-      const subChange = res[iMoney - coins[iCoin]]
+      const subChange = res[iMoney - coin]
 
       /* we check the change value for the current change + the coin 
       and update the res (for each change previously calculated)
       We filter the original null value from the parameter(first value of res) */
-      const newChg = subChange.map((x) => [...x, coins[iCoin]])
+      const newChg = subChange.map((x) => [...x, coin])
       // We add the new change to the res
       res[iMoney] = [...res[iMoney], ...newChg]
     }
 
-  return res[money].map((change) => change.slice(1))
+  return res[money]
 }
 
-const changeRecTer = (money, coins, indexCoin = 0, indexMoney = coins[0], result = [[[null]]].concat(Array(money).fill([]))) => {
+const changeRecTer = (money, coins, indexCoin = 0, indexMoney = coins[0], result = [[[]]].concat(Array(money).fill([]))) => {
   //Here , all the combination have been done we return the result(end of recursion)
   if (indexCoin == coins.length) return result[money]
   // If the array is not initialize , we create it
@@ -58,7 +58,7 @@ const changeRecTer = (money, coins, indexCoin = 0, indexMoney = coins[0], result
   /* we check the change value for the current chane + the coin 
   and update the result (for each change previously calculated)
   We filter the original null value from the parameter(first value of result) */
-  newChg = newChg.map((x) => x.concat(coins[indexCoin])).map((x) => x.filter((xx) => !!xx))
+  newChg = newChg.map((x) => x.concat(coins[indexCoin]))
   // We add the new change to the result
   result[indexMoney] = result[indexMoney].concat([...newChg])
   // We handle the indexes for the recursion
@@ -96,21 +96,21 @@ const result1 = changeRec(50, [2, 5, 7])
 console.timeEnd("changeRec")
 
 console.time("changeTab")
-// console.log(changeTab(120, [2, 5, 7]))
+// console.log(changeTab(20, [2, 5, 7]))
 const result2 = changeTab(50, [2, 5, 7])
 console.timeEnd("changeTab")
 
 console.time("changeTab2")
-// console.log(changeTab2(120, [2, 5, 7]))
+// console.log(changeTab2(20, [2, 5, 7]))
 const result2Bis = changeTab2(50, [2, 5, 7])
 console.timeEnd("changeTab2")
 
 console.time("changeHF")
-// console.log(changeHF(120, [2, 5, 7]))
+// console.log(changeHF(20, [2, 5, 7]))
 const result3 = changeHF(50, [2, 5, 7])
 console.timeEnd("changeHF")
 
 console.time("changeRecTer")
-// console.log(changeRecTer(120, [2, 5, 7]))
+// console.log(changeRecTer(12, [2, 5, 7]))
 const result4 = changeRecTer(50, [2, 5, 7])
 console.timeEnd("changeRecTer")
