f57933bb1176a1a197bf54600b741d3c2214c651
gary003 <gary.johnson003@gmail.com>
2021-07-31 18:21:11 +0200

    Add work on generators

--- combination.js
+++ combination.js
@@ -1,45 +1,66 @@
-const combinations = (cards, hand = [], allHands = [], shift = false) => {
-  if(cards.length == 0) return null
+const combinationsTree = (cards, hand = [], allHands = [], shift = false) => {
+  if (cards.length == 0) return null
 
   const [card, ...rest] = cards
-  
-  if(shift) allHands.push(hand)
 
-  if(hand.filter(x => x == card).length < cards.filter(x => x == card).length)
-    combinations(cards, hand.concat([card]),allHands , true)
-  
-  combinations(rest, hand, allHands,false)
-  
+  if (shift) allHands.push(hand)
+
+  if (hand.filter((x) => x == card).length < cards.filter((x) => x == card).length) combinationsTree(cards, hand.concat([card]), allHands, true)
+
+  combinationsTree(rest, hand, allHands, false)
+
   return allHands
 }
 
-function* iCombinations2rec(coins,change = [], shift = false){
+function* iCombinations2rec(coins, change = [], shift = false) {
   if (coins.length == 0) return null
-  
-  const [coin, ...rest] = coins  
-  
-  if(shift) yield change
-  
-  if(change.filter(x => x == coin).length < coins.filter(x => x == coin).length)
-    yield* [...iCombinations2rec(coins, change.concat([coin]) ,true)]
-  
-  yield* [...iCombinations2rec(rest, change, false)]  
+
+  const [coin, ...rest] = coins
+
+  if (shift) yield change
+
+  if (change.filter((x) => x == coin).length < coins.filter((x) => x == coin).length) yield* [...iCombinations2rec(coins, change.concat([coin]), true)]
+
+  yield* [...iCombinations2rec(rest, change, false)]
 }
 
-function* iCombinations1rec(arr, k,change = Array.from(arr)){
+function* iCombinations1rec(arr, k, change = Array.from(arr)) {
   if (k <= 0) return [undefined]
   if (k > arr.length) return null
-  if (k === 1) yield* change.map(x=>[x])
+  if (k === 1) yield* change.map((x) => [x])
   for (let i = 0; i < arr.length; ++i)
-    for (const x of iCombinations1rec(arr.slice(i+1),k-1)){
+    for (const x of iCombinations1rec(arr.slice(i + 1), k - 1)) {
       yield [change[i]].concat(x)
     }
 }
 
-console.time('e')
-const comb = combinations(['♣A','♦2','♥K','♠Q','♠9'])
-// const s = [...comb]
-console.log(comb)
-// console.log(s)
+function* iCombinationsTab(arr) {
+  const table = Array(arr.length + 1)
+  table[0] = [[]]
 
-console.timeEnd('e')
+  for (let i = 0; i < arr.length; i += 1) {
+    const subPlusNewCard = table[i].map((x) => [...x, arr[i]])
+    table[i + 1] = [...subPlusNewCard, ...table[i]]
+    yield* subPlusNewCard
+  }
+}
+
+console.time("combinationsTree")
+const comb1 = combinationsTree(["♣A", "♦2", "♥K", "♠Q", "♠9"])
+// console.log(comb1)
+console.timeEnd("combinationsTree")
+
+console.time("iCombinations2rec")
+const comb2 = [...iCombinations2rec(["♣A", "♦2", "♥K", "♠Q", "♠9"])]
+// console.log(comb2)
+console.timeEnd("iCombinations2rec")
+
+console.time("iCombinations1rec")
+const comb3 = [...iCombinations1rec(["♣A", "♦2", "♥K", "♠Q", "♠9"], 5)]
+// console.log(comb3)
+console.timeEnd("iCombinations1rec")
+
+console.time("iCombinationsTab")
+const comb4 = [...iCombinationsTab(["♣A", "♦2", "♥K", "♠Q", "♠9"])]
+// console.log(comb4)
+console.timeEnd("iCombinationsTab")
--- generators.js
+++ generators.js
@@ -1,20 +1,28 @@
-let iterator = "Qui?"[Symbol.iterator]() //?
+let iterator = "Qui?"[Symbol.iterator]()
 
-const sizes = ["tiny","little","medium","big","huge"]
-const astres = ["sun","comet","planet","nova"]
+const sizes = ["tiny", "little", "medium", "big", "huge"]
+const astres = ["sun", "comet", "planet", "nova"]
 
 const planetsMakerAsync = async function* () {
   while (Math.random() < 0.9) {
-    let size = sizes[Math.floor((Math.random() * sizes.length))]
-    let astretype = astres[Math.floor((Math.random() * astres.length))]
-    yield size+" "+astretype
+    let size = sizes[Math.floor(Math.random() * sizes.length)]
+    let astretype = astres[Math.floor(Math.random() * astres.length)]
+    yield size + " " + astretype
   }
 }
 
 async function example() {
   for await (const corps of planetsMakerAsync()) {
-    console.log(corps);
+    console.log(corps)
   }
 }
 
 example()
+
+const gen = function* () {
+  yield 0
+  yield [1, 2, 3]
+  yield* [1, 2, 3]
+}
+
+console.log(...gen())
